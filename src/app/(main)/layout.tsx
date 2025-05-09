e1e278c9f2e565cd1b6b9e5f09dda9aaf3d1dba8
Kartik Pawar <knpawar789@gmail.com>
2025-05-09 23:29:48 +0530

    feat: sidebar

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,15 @@
+import MenuOptions from "@/components/sidebar";
+import React from "react";
+
+type Props = { children: React.ReactNode };
+
+const Layout = ({ children }: Props) => {
+  return (
+    <div className="flex overflow-hidden h-screen">
+      <MenuOptions />
+      <div className="w-full">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
